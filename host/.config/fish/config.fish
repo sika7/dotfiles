d30a292999bb17e33dbfed89cef3a3273ae314b8
sika7 <91676209+sika7@users.noreply.github.com>
2022-05-11 17:00:32 +0900

    update fish config.

--- host/.config/fish/config.fish
+++ host/.config/fish/config.fish
@@ -1,8 +1,1 @@
-set PATH ~/.tools $PATH
 alias vim='nvim'
-alias dc='docker-compose'
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/basicexe/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/basicexe/Downloads/google-cloud-sdk/path.fish.inc'; end
-
-source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.fish.inc"
