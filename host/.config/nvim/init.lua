3e3ae76baecf83bd02532f0c32c6770b623dd326
sika7 <91676209+sika7@users.noreply.github.com>
2025-06-01 17:48:24 +0900

    nvimのgit操作をキーマップ

--- host/.config/nvim/init.lua
+++ host/.config/nvim/init.lua
@@ -303,7 +303,9 @@ vim.keymap.set("n", "<Leader>g", picker.grep)
 --==============================
 -- git差分表示のためのプラグイン
 --==============================
-require("gitsigns").setup({
+
+local gitsigns = require('gitsigns')
+gitsigns.setup({
   signs = {
     add = { text = "+" },
     change = { text = "~" },
@@ -312,6 +314,21 @@ require("gitsigns").setup({
     changedelete = { text = "~" },
   },
 })
+
+-- Actions
+vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk)
+vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk)
+vim.keymap.set('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+vim.keymap.set('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer)
+vim.keymap.set('n', '<leader>hu', gitsigns.undo_stage_hunk)
+vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer)
+vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk)
+vim.keymap.set('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+vim.keymap.set('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+vim.keymap.set('n', '<leader>hd', gitsigns.diffthis)
+vim.keymap.set('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+vim.keymap.set('n', '<leader>td', gitsigns.toggle_deleted)
 
 --==============================
 -- LSP Sever management
